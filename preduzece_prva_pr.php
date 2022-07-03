c1b0590e6da85384f664d4d8710ac4c247a3c11c
Marko Macura <marko.macura9@gmail.com>
2022-07-03 23:22:07 +0200

    update

--- preduzece_prva_pr.php
+++ preduzece_prva_pr.php
@@ -31,12 +31,23 @@
             <input type="submit" name="reset" value="Resetuj na jednu sifru delatnosti" />
             <br/>Sifre delatnosti:<br/>
             <?php
+            include_once './dbconnect.php';
             for($i=0; $i<$_SESSION['counter']; $i++){
                 ?>
                 &emsp;&emsp;<input type='text' name=<?php echo 'sif'.$i; ?>> <br/>
                 <?php
             }
+            if(isset($_POST['submit'])){
+                $kor_ime = $_SESSION['kor_ime'];
+                for($i=0; $i<$_SESSION['counter']; $i++){
+                    $temp = $_POST['sif'.$i];
+                    $result = mysqli_query($con,"INSERT INTO sifra_delatnosti(sifra, kor_ime) VALUES ('$temp', '$kor_ime')");
+                }
+                //echo 'PRCI KIKU BRE!';
+            }
+            mysqli_close($con);
             ?>
+            <input type="submit" name="submit" value="Pošalji" />
         </form>
     </body>
 </html>
